6b154e0769c4df988e52627b37e910963423ac23
hanan <hasyrawi@gmail.com>
2022-06-08 22:14:06 +0800

    reconfigure compiler

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.test.ts"],
+  verbose: true,
 };
